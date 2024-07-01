1d57a676324e265093d655e0ef221254ca88daf6
nive19s <sothilingamnivethan19@gmail.com>
2024-07-01 23:37:33 +0530

    created

---
+++ q3.scala
@@ -0,0 +1,13 @@
+object Question03{
+    def findMean(num1: Int,num2: Int): Double={
+       val average =( num1 + num2 )/2.0
+       average
+    }
+
+    def main (args :Array[String]) : Unit = {
+        val num1 = 8
+        val num2 = 4
+        val mean = findMean(num1,num2)
+        println(f"Mean of $num1 , $num2 is : $mean%.2f")
+    }
+}
